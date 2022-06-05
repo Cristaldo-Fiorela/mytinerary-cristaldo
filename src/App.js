92906e5e04ea6c6bd0d72233762fbd086a33eeee
Fiorela A Cristaldo <cristaldofiorela@gmail.com>
2022-06-05 20:54:19 -0300

    creacion de navBar y carrusel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Carousel from './component/Carousel';
 import NavBar from './component/Navbar';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <Carousel></Carousel>
     </div>
   );
 }
---
+++ src/component/Carousel.js
@@ -0,0 +1,45 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "../styles/carousel.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+function Carousel() {
+    return (
+        <>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                slidesPerGroup={3}
+                loop={true}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide></SwiperSlide>
+                <SwiperSlide></SwiperSlide>
+                <SwiperSlide></SwiperSlide>
+                <SwiperSlide></SwiperSlide>
+                <SwiperSlide></SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
+        </>
+    );
+}
+
+export default Carousel
