4634d6705c30196657024104cbde711c3266e26d
Fiorela A Cristaldo <cristaldofiorela@gmail.com>
2022-06-08 21:20:18 -0300

    correcion de colores de carrusel

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,7 +7,6 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
@@ -30,7 +29,7 @@ const pages = [
     }
 ];
 
-const settings = ['Profile', 'Account', 'Dashboard'];
+const settings = ['Settings', 'LogIn'];
 
 const ResponsiveAppBar = () => {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
